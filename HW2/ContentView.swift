c2c77b9ff6e1a5dec38979f8af015a858764cc28
listen <jc91110@gmail.com>
2020-04-15 02:33:18 -0700

    Initial Commit

---
+++ HW2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW2
+//
+//  Created by WRYYYY on 2020/4/15.
+//  Copyright © 2020 WRYYYY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
